7889c0ea9687cb7c1324ca3859987c4821556b05
laimatt <laimat78@gmail.com>
2024-08-01 13:43:35 -0400

    added subsample functionality

--- pdal_script.py
+++ pdal_script.py
@@ -19,19 +19,30 @@ class PointCloudPostProcessor:
     def __init__(self, training):
         self.training = training
 
-    def lasToCsv(self, test_dir, min_p, tolerance, max_p, file_name):
+    def lasToCsv(self, test_dir, min_p, tolerance, max_p, subsample, file_name):
         # pdal = "/home/roboticslab/Developer/laimatt/laimatt_pdal/.conda/bin/pdal"
         pdal = "pdal"
+        
         command = [
             pdal,
             "translate",
             file_name,
+            test_dir + "/subsample.las",
+            "sample",
+            f"--filters.sample.radius={subsample}"
+        ]
+        result = subprocess.run(command, capture_output=True, text=True, check=True)
+        
+        command = [
+            pdal,
+            "translate",
+            test_dir + "/subsample.las",
             test_dir + "/full_segmented.csv",
             "-f",
             "filters.cluster",
-            "--filters.cluster.min_points=" + str(min_p),
-            "--filters.cluster.tolerance=" + str(tolerance),
-            "--filters.cluster.max_points=" + str(max_p) 
+            f"--filters.cluster.min_points={min_p}",
+            f"--filters.cluster.tolerance={tolerance}",
+            f"--filters.cluster.max_points={max_p}"
         ]
         result = subprocess.run(command, capture_output=True, text=True, check=True)
         
@@ -106,14 +117,15 @@ class PointCloudPostProcessor:
             path_poly = test_dir + "/component_poly_" + test_index
             os.makedirs(path_las)
             os.makedirs(path_poly)
+            
+            # pdal = "/home/roboticslab/Developer/laimatt/laimatt_pdal/.conda/bin/pdal"
+            pdal = "pdal"
         
             for x in range(len(clusters)):
                 csv_file = test_dir + "/component_csv/component_" + f"{x:06d}" + ".csv"
                 las_file = path_las + "/component_" + f"{x:06d}" + ".las"
                 asc_file = path_poly + "/component_" + f"{x:06d}" + ".poly"
-                
-                # pdal = "/home/roboticslab/Developer/laimatt/laimatt_pdal/.conda/bin/pdal"
-                pdal = "pdal"
+
                 command = [
                     pdal,
                     "translate",
@@ -126,11 +138,10 @@ class PointCloudPostProcessor:
             
                 
                     
-        line_list = []
-        for index in range(len(clusters)):
-            line_list.append(self.csv_to_asc(csv_file, las_file, asc_file, clusters, index))
-            # if index == 6: line_list.append(self.csv_to_asc(csv_file, las_file, asc_file, clusters, index))
-        return line_list
+        return [
+            self.csv_to_asc(csv_file, las_file, asc_file, clusters, index)
+            for index in range(len(clusters))
+            ]
     
     def csv_to_asc(self, csv_file, las_file, asc_file, clusters, index):
         
@@ -151,7 +162,7 @@ class PointCloudPostProcessor:
         # print(np_coords[source])
         
         nearest_neighbors_path = self.find_nearest_neighbors(np_coords, np_coords[source])
-        print(nearest_neighbors_path)
+        # print(nearest_neighbors_path)
 
         if not self.training:
             # Open the ASC file for writing
@@ -160,13 +171,11 @@ class PointCloudPostProcessor:
                     # Join x, y, z with a space and write to the ASC file
                     f.write(f"{index[0]} {index[1]} {index[2]}\n")
                     
-        path = []
-        for index in nearest_neighbors_path:
-            print(numpy.array(index))
-            path.append(numpy.array(index))
+        
+        path = [numpy.array(index) for index in nearest_neighbors_path]
             
-        print(path)
-        print(numpy.asarray(path))
+        # print(path)
+        # print(numpy.asarray(path))
         return numpy.asarray(path)
                 
     def nearestToCorner(self, corners, points):
@@ -175,7 +184,10 @@ class PointCloudPostProcessor:
         nbrs = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(ptsWcorners)
         distances, indices = nbrs.kneighbors(corners)
         boxpoints = numpy.concatenate((indices, distances), axis = 1)
-        min_point = int(numpy.squeeze(numpy.where(boxpoints[:8, 3] == min(boxpoints[:8, 3]))[0]))
+        
+        print(numpy.where(boxpoints[:8, 3] == min(boxpoints[:8, 3]))[0][0])
+        # min_point = int(numpy.squeeze(numpy.where(boxpoints[:8, 3] == min(boxpoints[:8, 3]))[0]))
+        min_point = numpy.where(boxpoints[:8, 3] == min(boxpoints[:8, 3]))[0][0]
         
         # print(boxpoints)
         # print(boxpoints[:8, 3])
@@ -230,7 +242,7 @@ class PointCloudPostProcessor:
         visited = set()
         visited.add(tuple(current_point))
 
-        for x in range(len(points) - 1):
+        for _ in range(len(points) - 1):
             # Find the nearest neighbor
             distances, indices = nbrs.kneighbors([current_point])
             next_closest = 1
@@ -312,7 +324,7 @@ class PointCloudPostProcessor:
         
         for filepath in filepaths[1:]:
             b = self.bounding_box_info(filepath)
-            link = "https://laimatt.boshang.online/download/" + str(project_id) + "/" + task_id + "/" + os.path.basename(filepath)
+            link = f"https://laimatt.boshang.online/download/{str(project_id) }/" + task_id + "/" + os.path.basename(filepath)
             
             query = "INSERT INTO patch_crack (center_lat, center_long, center_alt, box_length, box_width, box_height, type, las_link, whole_data_id) " + \
                 "VALUES ('%s', '%s', '%s', '%s', '%s', '%s', %s, %s, %s)"
@@ -324,64 +336,70 @@ class PointCloudPostProcessor:
         mydb.close()    
         
     def populate_csv(self, test_dir, test_index):
-        csvoutput = open(test_dir + '/component_data.csv', 'w', newline='')
-        writer = csv.writer(csvoutput)
-        writer.writerow(['x', 'y', 'z', 'length', 'width', 'height', 'type', 'original file'])
-        
-        for filepath in sorted(glob.iglob(test_dir + '/component_las_' + test_index + '/*')):
-            box_info = self.bounding_box_info(filepath)
-            writer.writerow(box_info + ['crack', filepath])
-        
-        csvoutput.close()
+        with open(test_dir + '/component_data.csv', 'w', newline='') as csvoutput:
+            writer = csv.writer(csvoutput)
+            writer.writerow(['x', 'y', 'z', 'length', 'width', 'height', 'type', 'original file'])
             
-    def create_components(self, project_id, task_id, uid, color, min_p, tolerance, max_p, file_name, test_path): 
+            for filepath in sorted(glob.iglob(test_dir + '/component_las_' + test_index + '/*')):
+                box_info = self.bounding_box_info(filepath)
+                writer.writerow(box_info + ['crack', filepath])
+            
+    def create_components(self, project_id, task_id, uid, color, min_p, tolerance, max_p, subsample, file_name, test_path): 
         
             
-        test_dir = os.path.join(test_path, ('{}_test_{}_{}_{}').format(getName(TypeColor, color), min_p, tolerance, max_p))
+        test_dir = os.path.join(test_path, (f'{getName(TypeColor, color)}_test_{min_p}_{tolerance}_{max_p}'))
         # test_dir = os.path.join(test_path, ("test_" + str(min_p) + "_" + str(tolerance) + "_" + str(max_p)))
-        test_index = str(min_p) + "_" + str(tolerance) + "_" + str(max_p)
+        test_index = f"{str(min_p)}_{str(tolerance)}_{str(max_p)}"
         if os.path.exists(test_dir):
             print(test_dir + " already exists, remaking", flush=True)
             shutil.rmtree(test_dir)
         os.makedirs(test_dir)
 
 
-        self.lasToCsv(test_dir, min_p, tolerance, max_p, file_name)
+        self.lasToCsv(test_dir, min_p, tolerance, max_p, subsample, file_name)
         clusters = self.create_csvs(test_dir)
         result = self.csvToLas(test_dir, test_index, clusters)
         
         print(result)
-
-        # self.populate_db(test_dir, test_index, uid, project_id, task_id, color)
-        self.populate_csv(test_dir, test_index)
+        
+        if not self.training:
+            # self.populate_db(test_dir, test_index, uid, project_id, task_id, color)
+            self.populate_csv(test_dir, test_index)
         return "success"
 
 @app.route('/components', methods=['GET'])
 def components_api():
-    project_id = 99
-    task_id = "aa"
+    project_id = 000
+    task_id = "test"
     uid = 12
     color = 2
-    min_p = 10
-    tolerance = .2
+    min_p = 4
+    tolerance = .65
     max_p = 10000
+    subsample = .2
     
-    folder_path = 'tasks/task_{}_{}/'.format(project_id, task_id)
+    folder_path = f'tasks/task_{project_id}_{task_id}/'
     test_path = os.path.join(folder_path, "training" if ppp.training else "tests")
         
     # file_name = os.path.join(folder_path, '{}_filtered_model.las'.format(getName(TypeColor, color)))
-    file_name = "blue.las"        
+    # file_name = "blue.las"
+    file_name = 'subsample.las'
+    
     if not (os.path.exists(test_path)):
         os.makedirs(os.path.join(test_path))
             
-    processed_data = ppp.create_components(project_id, task_id, uid, color, min_p, tolerance, max_p, file_name, test_path)
+    processed_data = ppp.create_components(
+        project_id, task_id, uid, 
+        color, 
+        min_p, tolerance, max_p, subsample, 
+        file_name, test_path)
 
     return processed_data
 
 @app.route('/download/<project_id>/<task_id>/<filename>', methods=['GET'])
 def download(project_id, task_id, filename):
     # Assuming files are stored in a directory named 'files' under the app root directory
-    task = os.path.join(app.root_path, 'task_{}_{}'.format(project_id, task_id))
+    task = os.path.join(app.root_path, f'task_{project_id}_{task_id}')
     
     uploads = os.path.join(task, 'tests/test_10_0.2_10000/component_las_10_0.2_10000')
 
@@ -389,6 +407,6 @@ def download(project_id, task_id, filename):
     return send_file(os.path.join(uploads, filename), as_attachment=True)
 
 
-ppp = PointCloudPostProcessor(False)
+ppp = PointCloudPostProcessor(True)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=2001, debug=True)
